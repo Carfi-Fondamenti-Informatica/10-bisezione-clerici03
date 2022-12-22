a7edfb37cfc8095f2b937dd0ace82b12c1816ddd
clerici03 <115361946+clerici03@users.noreply.github.com>
2022-12-22 22:54:08 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,12 +13,12 @@ int main() {
     do{
         cout << " inserire estremi" << endl;
         cin >> a >> b;
-    }while (f(a) * f(b) >= 0);
+        }while (f(a) * f(b) >= 0);
     do {
         x=(a+b)/2;
         if (f(x)==0){
-            cout << x << endl;
-            cout << f(x) << endl;
+            cout<< x <<endl;
+            cout<< f(x) <<endl;
             return 0;
         }else{
             if(f(a)*f(b)<0){
@@ -26,9 +26,9 @@ int main() {
             }else{
                 a=x;
             }
-            err =abs((b-a)/2);
+            err = abs ((b-a)/2);
         }
-    }while (err>= 1e-6);
+    }while (err >= 1e-6);
     cout << int(x*10000)/10000 << endl;
     return 0;
 }
